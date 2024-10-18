95ffb2691a520321f658df3e8c06440302360411
jcedeno <jcedeno.us+infinity@gmail.com>
2024-10-17 22:15:56 -0400

    feat: more productionalized

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4a5568",
+        secondary: "#718096",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
+  },
+};
